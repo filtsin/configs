6d67b23a6f03a5f652f0b99873505f220028df69
Ilya Filtsin <ilfiltsin@gmail.com>
2023-03-06 20:24:40 +0300

    Update nvim saga configs

--- nvim/lua/configs/lspsaga.lua
+++ nvim/lua/configs/lspsaga.lua
@@ -3,23 +3,26 @@ local setup = function()
 
     require('lspsaga').setup {
         outline = {
-            auto_preview = false
+            win_position = "left",
+            win_width = 40,
         },
         diagnostic = {
             on_insert = false
         },
         lightbulb = {
             enable = false
-        }
+        },
     }
 
+    keymap('n', 'gh', '<cmd>Lspsaga lsp_finder<CR>')
     keymap('n', '<space>a', '<cmd>Lspsaga code_action<CR>')
     keymap('n', '<leader>d', '<cmd>Lspsaga goto definition<CR>')
     keymap('n', '<leader>rn', '<cmd>Lspsaga rename ++project<CR>')
+    keymap('n', '<leader>o', '<cmd>Lspsaga outline<CR>')
 
 
     keymap("n", "<leader>n", function()
-        require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.ERROR })
+        require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR })
     end)
 end
 
