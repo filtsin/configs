684715bbbc7cdf8887e3deb9984dc80d3312bc6d
Ilya Filtsin <ilfiltsin@gmail.com>
2024-06-21 01:48:02 +0300

    Dotter manager: yofi + waybar

---
+++ pacman.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Not full list
+paru -S --neded
+    dotter-rs # config manager
+    hyprcursor # cursor theme configuration
+    phinger-cursors # cursor theme
+    hyprland # hyprland :)
+    adwaita-icon-theme # icon theme for yofi
+    xdg-desktop-portal-hyprland # portal for screen capture
+    ttf-jetbrains-mono # jetbrains font
+    noto-fonts-emoji # emoji
+    
